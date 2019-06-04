615f816f2f0475127e5b800c93b1fa6c2f22a780
Acep Triana <49948930+aceptriana@users.noreply.github.com>
2019-06-04 18:23:38 +0700

    Add files via upload

---
+++ web.sh
@@ -0,0 +1,14 @@
+figlet -f future "Web Target"
+sleep 1
+
+echo "Langkah - langkahnya"
+echo ""
+echo "[1] Pilih web nya"
+echo "[2] Terus copy"
+echo "[3] Ctrl X "
+echo "[4] Terus ketik sh webdav.sh"
+echo "[5] Masukan scrift ,paste kan webnya"
+echo ""
+echo "Apakah anda paham?"
+sleep 5
+nano web.txt
